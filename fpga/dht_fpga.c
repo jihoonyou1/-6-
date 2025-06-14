343e323f4629e37fd93ada5757e90acd298aa297
jinwoo <79890974+rlawlsdn77@users.noreply.github.com>
2025-06-14 15:30:27 +0900

    Update dht_fpga.c

--- fpga/dht_fpga.c
+++ fpga/dht_fpga.c
@@ -8,7 +8,7 @@
 #include <signal.h>  // For signal handling
 
 #define MAX_TIMINGS 85
-#define DHT_PIN 2         // WiringPi pin for DHT22 (GPIO27) - 이 값은 실제 연결된 핀 번호로 확인해주세요.
+#define DHT_PIN 5        // WiringPi pin for DHT22 (GPIO27) - 이 값은 실제 연결된 핀 번호로 확인해주세요.
 #define FPGA_TEXT_LCD_DEVICE "/dev/fpga_text_lcd" // FPGA TEXT LCD device path
 #define FPGA_LED_DEVICE "/dev/fpga_led" // FPGA LED device path
 #define MAX_BUFF 32       // Total buffer size for LCD (2 lines * 16 chars)
